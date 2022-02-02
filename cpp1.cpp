40d215caa61565942a9a06a0a7244a4e3f45d048
fansan <fanin.s.pbl@gmail.com>
2022-02-02 09:21:28 +0530

    New Repository created for learning and testing of C++ Programs

---
+++ cpp1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>d
+using namespace std;                                    
+int main(){
+    int x,y;
+    cout<<"***********************************"<<endl;
+    cout<<"Type two Numbers : ";
+    cin>>x>>y;
+    cout<<"sum of numbers is: "<<x+y<<endl;            //both endl and 
+    cout<<"product of numbers: "<<x*y<<"\n";          //"\n" can be use for new line.
+    cout<<"subtration of numbers: "<<x-y<<endl;
+    cout<<"division of numbers: "<<x/y<<"\n";
+    cout<<"***********************************";
+} 
---
+++ intstd/cpptest.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string name;
+    cout<<"Hello C++\n";
+    cin>>name;
+    cout<<"Hello "<<name;
+}
