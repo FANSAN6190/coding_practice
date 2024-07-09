6ee075208d48a9a9b7912de25d2c4559d3650c7c
Fanindra Saini <fanin.s.pbl@gmail.com>
2024-07-09 23:06:47 +0530

    Time: 12 ms (92.70%), Space: 44.4 MB (39.93%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -4,31 +4,43 @@ class Solution {
         Arrays.sort(nums);
         List<List<Integer>> fourS = new ArrayList<>(); 
         Set<List<Integer>> tempSet = new HashSet<>();
-        for(int i=0; i<n; i++){
+        for(int i=0; i<n-1; i++){
+            if(i>0 && nums[i]==nums[i-1]){
+                // System.out.println("in");
+                continue;
+            }
+            // System.out.println("out");
             int j=i+1;
             int k=j+1;
             while(j<k){
-                k=j+1;
-                int l=n-1;
-                while(k<l){
-                    long sum = (long)nums[i]+(long)nums[j]+(long)nums[k]+(long)nums[l];
-                    if(sum<target){
-                        k++;
-                    }
-                    else if(sum>target){
-                        l--;
-                    }
-                    else if(sum==target){
-                        List<Integer> list = new ArrayList<>();
-                        list.add(nums[i]);
-                        list.add(nums[j]); 
-                        list.add(nums[k]);
-                        list.add(nums[l]);
-                        tempSet.add(list);
-                        k++;
-                        l--;
-                    }
+                if(j>i+1 && nums[j]==nums[j-1]){
+                    j++;
+                    continue;
                 }
+                    k=j+1;
+                    int l=n-1;
+                    while(k<l){
+                        long sum = (long)nums[i]+nums[j]+nums[k]+nums[l];
+                        if(sum<target){
+                            k++;
+                            while(k<=l && nums[k]==nums[k-1]){
+                                k++;
+                            }
+                        }
+                        else if(sum>target){
+                            l--;
+                        }
+                        else if(sum==target){
+                            List<Integer> list = new ArrayList<>();
+                            list.add(nums[i]);
+                            list.add(nums[j]); 
+                            list.add(nums[k]);
+                            list.add(nums[l]);
+                            tempSet.add(list);
+                            k++;
+                            l--;
+                        }
+                    }
                 j++;
             }
         }
