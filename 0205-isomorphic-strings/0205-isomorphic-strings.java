647324866794eb4313d0465a233ba8b930c4300e
Fanindra Saini <fanin.s.pbl@gmail.com>
2024-08-02 12:43:29 +0530

    Time: 16 ms (29.78%), Space: 42.5 MB (33.16%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -3,11 +3,13 @@ class Solution {
         Map<Character,Character> charMap1 = new HashMap<>();
         Map<Character,Character> charMap2 = new HashMap<>();
         for(int i=0;i<s.length();i++){
-            charMap1.putIfAbsent(s.charAt(i),t.charAt(i));
-            charMap2.putIfAbsent(t.charAt(i),s.charAt(i));
+            charMap1.put(s.charAt(i),t.charAt(i));
+            charMap2.put(t.charAt(i),s.charAt(i));
+        }
+        for(int i=0;i<s.length();i++){
             if(charMap1.get(s.charAt(i))!=t.charAt(i) || charMap2.get(t.charAt(i))!=s.charAt(i)){
                 return false;
-            }
+            }        
         }
         return true;
     }
