52e0c8677d3903106d548e371140e638baf73276
Fanindra Saini <fanin.s.pbl@gmail.com>
2024-08-02 12:42:48 +0530

    Time: 25 ms (8.16%), Space: 42.4 MB (47.79%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -5,11 +5,9 @@ class Solution {
         for(int i=0;i<s.length();i++){
             charMap1.putIfAbsent(s.charAt(i),t.charAt(i));
             charMap2.putIfAbsent(t.charAt(i),s.charAt(i));
-        }
-        for(int i=0;i<s.length();i++){
             if(charMap1.get(s.charAt(i))!=t.charAt(i) || charMap2.get(t.charAt(i))!=s.charAt(i)){
                 return false;
-            }        
+            }
         }
         return true;
     }
