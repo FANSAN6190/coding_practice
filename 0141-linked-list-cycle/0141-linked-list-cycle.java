49c7fc7953ecb596aa3f2a209156569cfa68d80f
Fanindra Saini <fanin.s.pbl@gmail.com>
2024-08-07 22:36:01 +0530

    Time: 0 ms (100.00%), Space: 45 MB (6.32%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,14 +11,24 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        Set<ListNode> hash = new HashSet<>();
-        ListNode temp = head;
-        while(temp!=null){
-            if(hash.contains(temp)){
+        ListNode rabb = head;
+        ListNode tort = head;
+        if(head==null || head.next==null || head.next.next == null){
+            return false;
+        }
+        else if(head.next==head || head.next.next == head){
+            return true;
+        }
+        while(rabb!=null){
+            // System.out.println(rabb.val+" "+tort.val);
+            if(rabb.next==null){
+                return false;
+            }
+            tort = tort.next;
+            rabb = rabb.next.next;
+            if(rabb==tort){
                 return true;
             }
-            hash.add(temp);
-            temp=temp.next;
         }
         return false;
     }
